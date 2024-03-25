0bf1334d009e7428d72c0e6158c53727b99a021b
Ashutosh Gorke <ashutosh@technative.in>
2024-03-25 21:27:01 +0530

    first commit

---
+++ client/src/app/page.js
@@ -0,0 +1,50 @@
+'use client';
+import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+const { io } = require("socket.io-client");
+import axios from "axios";
+
+export default function Home() {
+  let socket = null;
+  const [socketID, setSocketID] = useState(null);
+  const [logData, setLogData] = useState(null);
+
+  useEffect(() => {
+    socket = io("http://192.168.31.112:5000");
+
+    socket.on("connect", () => {
+      console.log("Connected to the server with socket ID:", socket.id);
+      setSocketID(socket.id);
+    });
+
+    socket.on("file-update", (data) => {
+      console.log("File Update", data);
+      setLogData(prevData => [prevData, ...data.join("\n")]);
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (socketID) {
+      getFileLogs();
+    }
+  }, [socketID]);
+
+  async function getFileLogs () {
+    try {
+      let response = await axios.get(`http://192.168.31.112:5000/log?userID=${socketID}`);
+      console.log("Data", response.data);
+      setLogData(response.data);
+    } catch (Exception) {
+      console.log(Exception);
+    }
+  }
+
+  return (
+    <main className={styles.main}>
+      <pre>{logData}</pre>
+    </main>
+  );
+}
---
+++ server/index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+const fs = require("fs");
+const ftShowLineLimit = 11;
+const { v4: uuidv4 } = require('uuid');
+const { Server } = require("socket.io");
+var cors = require('cors');
+const connectedClient = {};
+
+app.use(cors());
+app.get("/", (req, res) => {
+    return res.send("Home");
+});
+
+app.get("/log", async (req, res) => {
+    try {
+        let { userID } = req.query;
+        let fileContent = await fs.readFileSync("/home/ashutosh/Learning/sockets/File Monitoring System/test.txt", "utf-8");
+        let lines = fileContent.split("\n");
+        connectedClient[userID] = lines.length;
+        let response = lines.slice(-ftShowLineLimit).join("\n");
+        console.log(connectedClient);
+        return res.send(response);
+    } catch (Exception) {
+        console.log(Exception);
+        return res.json({ message: "Something went wrong"});
+    }
+});
+
+const expressServer = app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
+
+const io = new Server(expressServer, {
+    cors: {
+        origin: "*"
+    }
+});
+
+const serverSocket = io.on("connection", socket => {
+    console.log(`User ${socket.id} connected`);
+
+    //When user disconnects
+    socket.on("disconnect", () => {
+        delete connectedClient[socket.id];
+        console.log(`User ${socket.id} disconnected`);
+    })
+});
+
+fs.watch("../test.txt", async (eventType, filename) => {
+    let fileContent = await fs.readFileSync("/home/ashutosh/Learning/sockets/File Monitoring System/test.txt", "utf-8");
+    let lines = fileContent.split("\n");
+    console.log(Object.keys(connectedClient))
+    let clientIDs = Object.keys(connectedClient);
+    if (clientIDs.length) {
+        clientIDs.forEach(clientID => {
+        let changedContent = lines.slice(connectedClient[clientID], lines.length);
+            if (changedContent.length) {
+                serverSocket.to(clientID).emit("file-update", changedContent);  
+                connectedClient[clientID] = lines.length;
+            }
+        });
+    }
+}); 
